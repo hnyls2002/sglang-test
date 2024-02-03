ad47e2d97fadd20aab7006229e57c9c51c4bcf2d
hnyls2002 <hnyls2002@gmail.com>
2024-02-03 12:43:39 +0000

    update plot

--- demo/plot.py
+++ demo/plot.py
@@ -1,7 +1,8 @@
 import argparse
-import matplotlib.pyplot as plt
 import json
 from collections import defaultdict
+
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -21,16 +22,6 @@ def show_name(name):
     return convert.get(name, name)
 
 
-def get_hatch(backend):
-    convert = {
-        "srt": "",
-        "srt_no_fst_fwd": "*",
-        "vllm": "",
-        "guidance": "",
-    }
-    return convert.get(backend, "")
-
-
 def get_color(backend):
     convert = {
         "srt": "C0",
@@ -44,50 +35,21 @@ def get_color(backend):
 backend_list = ["srt", "srt_no_fst_fwd", "vllm", "guidance"]
 
 
-def plot_group_bar_chart(
-    ax, data, tasks, methods, width=0.1, y_label="", baseline=None
-):
-    # Organize data
-    group_labels = [show_name(t) for t in tasks]
-    groups = []
-    for method in methods:
-        tmp = []
-        for task in tasks:
-            value = data[task].get(method, 0)
-            if baseline is not None:
-                value /= data[task][baseline]
-            tmp.append(value)
-        groups.append(tmp)
-
-    # Draw bar
-    x = np.arange(len(group_labels))
-    for i, g in enumerate(groups):
-        ax.bar(
-            x + i * width,
-            g,
-            width,
-            label=show_name(methods[i]),
-            # hatch=get_hatch(methods[i]),
-            color=get_color(methods[i]),
-        )
-
-    # Add some text for labels, title and custom x-axis tick labels, etc.
-    ax.set_ylabel(y_label, fontsize=15)
-    if baseline is not None:
-        ax.set_ylim(ymax=1.1)
-        ax.set_yticks([0, 0.25, 0.5, 0.75, 1])
-    ax.set_xticks(x + (len(groups) - 1) / 2 * width)
-    ax.set_xticklabels(group_labels, fontsize=15)
-    # x tick fontsize
-    for tick in ax.yaxis.get_major_ticks():
-        tick.label1.set_fontsize(15)
-    ax.legend(
-        handlelength=0.7,
-        ncols=4,
-        loc="lower center",
-        bbox_to_anchor=(0.5, -0.45),
-        fontsize=13,
+def plot_one_chat(ax, x_names, data, title, width=0.7, y_lable=""):
+    x_pos = np.arange(len(x_names))
+    vals = [data[x] for x in x_names]
+    ax.bar(
+        x_pos,
+        vals,
+        width,
+        label=[show_name(x) for x in x_names],
+        color=[get_color(x) for x in x_names],
     )
+
+    # remove the xticks
+    ax.set_xticks([])
+    ax.set_ylabel(y_lable, fontsize=15)
+    ax.set_title(title, fontsize=15)
     ax.grid(True, which="both", linestyle="--", linewidth=0.5)
     ax.set_axisbelow(True)
 
@@ -115,29 +77,60 @@ def main():
     data_ct = defaultdict(lambda: defaultdict(lambda: 0))
     for res in results:
         if res["parallel"] == max_parallel[res["backend"]]:
-            data[res["mode"]][res["backend"]] += res["latency"]
+            data[res["mode"]][res["backend"]] += res["num_jsons"] / res["latency"]
             data_ct[res["mode"]][res["backend"]] += 1
         if res["parallel"] == 1:
-            data[res["mode"] + "_bs1"][res["backend"]] += res["latency"]
+            data[res["mode"] + "_bs1"][res["backend"]] += (
+                res["latency"] / res["num_jsons"]
+            )
             data_ct[res["mode"] + "_bs1"][res["backend"]] += 1
 
     for bm in data:
         for backend in data[bm]:
             data[bm][backend] /= data_ct[bm][backend]
             print(bm, backend, data[bm][backend])
-            data[bm][backend] = 1 / data[bm][backend]
 
     # get one figure
-    fig, ax = plt.subplots(1, 1, figsize=(13, 4))
-    plot_group_bar_chart(
-        ax,
-        data,
-        ["character", "city", "character_bs1", "city_bs1"],
-        backend_list,
-        y_label="Normalized Throughput",
-        baseline="srt",
+    fig, axs = plt.subplots(1, 4, figsize=(16, 4))
+    plot_one_chat(
+        axs[0],
+        ["srt", "srt_no_fst_fwd", "vllm", "guidance"],
+        data["character"],
+        "Character Generation",
+        y_lable="Throughput (Req / s)",
     )
-    plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.3)
+    plot_one_chat(
+        axs[1],
+        ["srt", "srt_no_fst_fwd", "vllm", "guidance"],
+        data["city"],
+        "Long Document Retrieval",
+        y_lable="Throughput (Req / s)",
+    )
+    plot_one_chat(
+        axs[2],
+        ["srt", "srt_no_fst_fwd", "vllm", "guidance"],
+        data["character_bs1"],
+        "Character Generation\n(Batch Size = 1)",
+        y_lable="Latency (s / Req)",
+    )
+    plot_one_chat(
+        axs[3],
+        ["srt", "srt_no_fst_fwd", "vllm", "guidance"],
+        data["city_bs1"],
+        "Long Document Retrieval\n(Batch Size = 1)",
+        y_lable="Latency (s / Req)",
+    )
+
+    plt.subplots_adjust(wspace=0.4, bottom=0.2)
+    handles, labels = axs[0].get_legend_handles_labels()
+    fig.legend(
+        handles,
+        labels,
+        loc="lower center",
+        ncol=4,
+        bbox_to_anchor=(0.5, 0),
+        fontsize=16,
+    )
 
     fig.savefig("result.png")
 
